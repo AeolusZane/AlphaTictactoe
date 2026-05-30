51c87c0780e4e4ffb4d4a65f11c526033849e52e
AeolusZane <aeoluj@gmail.com>
2026-05-31 05:55:27 +0800

    feat: 添加 MCTS 可视化视频

--- training-video/src/Root.tsx
+++ training-video/src/Root.tsx
@@ -1,6 +1,7 @@
 import { Composition } from "remotion";
 import { TicTacToeTraining } from "./TicTacToeTraining";
 import { WeightUpdateVisualization } from "./WeightUpdateVisualization";
+import { MCTSVisualization } from "./MCTSVisualization";
 
 export const RemotionRoot = () => {
   return (
@@ -17,6 +18,14 @@ export const RemotionRoot = () => {
         id="WeightUpdate"
         component={WeightUpdateVisualization}
         durationInFrames={3150}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="MCTSVisualization"
+        component={MCTSVisualization}
+        durationInFrames={3600}
         fps={30}
         width={1920}
         height={1080}
